39518a3b2bdd012cf181f1b0c0ecc5033f044588
Anton Evdokimov <free0u@gmail.com>
2014-12-20 14:04:51 +0300

    Added read function, modify image class(toString)

--- Lab7/src/Image.java
+++ Lab7/src/Image.java
@@ -8,7 +8,7 @@ public class Image {
     public Image(byte[] d, byte type) {
         data = new double[d.length];
         for (int i = 0; i < data.length; i++) {
-            byte t = d[i];
+            int t = d[i] & 0xFF;
             data[i] = (double) t / 255;
         }
         this.type = type;
@@ -19,10 +19,15 @@ public class Image {
         StringBuilder res = new StringBuilder();
         res.append(type);
         res.append("\n");
+        int r = 0;
         for (int i = 0; i < data.length; i++) {
-            res.append(data[i] < 0.5 ? 0 : 1);
-            if (i % 28 == 0) {
+//            res.append(data[i] < 0.5 ? 0 : 1);
+            int t = (int)(data[i] * 10) % 10;
+            res.append(t == 0 ? " " : t);
+            r++;
+            if (r == 28) {
                 res.append("\n");
+                r = 0;
             }
         }
         return res.toString();
---
+++ Lab7/src/Main.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by free0u on 12/20/14.
+ */
+public class Main {
+    public static List<Image> readImages(String name) throws IOException {
+        List<Image> res = new ArrayList<>();
+
+        byte[] rawLabels = Files.readAllBytes(Paths.get(name + ".labels"));
+        byte[] rawData = Files.readAllBytes(Paths.get(name + ".data"));
+
+        byte[] rawLen = Arrays.copyOfRange(rawLabels, 4, 8);
+        ByteBuffer buf = ByteBuffer.wrap(rawLen);
+
+        int countElements = buf.getInt();
+
+        for (int i = 0; i < countElements; i++) {
+            byte label = rawLabels[i + 8];
+
+            int startInd = 16 + i * 28 * 28;
+            int endInd = startInd + 28 * 28;
+
+            byte[] image = Arrays.copyOfRange(rawData, startInd, endInd);
+
+            res.add(new Image(image, label));
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        List<Image> trainImages = readImages("train");
+        List<Image> testImages = readImages("test");
+    }
+}
